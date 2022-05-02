0413e14def9f48033f28bae7af52c3077e8f9292
luiscaputo <luiscaputo15@gmail.com>
2022-05-02 03:22:41 +0100

    :zap: Configurating Routes Setup and making validate route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const Router = require('express');
+const ValidateAngolanPhoneController = require('../controllers/validateAngolanPhone.controller'); 
+const validateAngolanPhoneController = new ValidateAngolanPhoneController();
 
 const router = Router();
 
 router.get('/', (req, res) =>{
     res.status(200).json('Server is running');
-})
+});
+
+router.get('/validate/:phone', validateAngolanPhoneController.handle);
 
 module.exports = router;
