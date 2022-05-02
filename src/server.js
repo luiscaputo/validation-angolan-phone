a2fe11c645cdb3e394ba09d9d1de23d20d3452b0
luiscaputo <luiscaputo15@gmail.com>
2022-05-02 03:23:12 +0100

    :zap: Server Configured Like accept Json data

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
