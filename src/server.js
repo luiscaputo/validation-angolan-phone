076ed57d842b119b786d09c99512d2b1c234600b
luiscaputo <luiscaputo15@gmail.com>
2022-05-02 02:32:59 +0100

    :tada: Starting Project

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
